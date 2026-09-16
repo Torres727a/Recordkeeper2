ff0d18e2444ccdfc806612fea519573f35214620
Torres <25-000934@mcstayuman.edu.ph>
2026-09-16 17:16:50 +0800

    Added Torres profile test

--- app/src/test/java/com/example/recordkeeper/quarter2/torresProfileTest.java
+++ app/src/test/java/com/example/recordkeeper/quarter2/torresProfileTest.java
@@ -6,7 +6,7 @@ public class torresProfileTest {
     @Test
     public void printMyProfile() {
         String myName = "Lance";
-        String petName = "Cervantes";
+        String petName = "Cervantess";
         String favFood = "Pagpag";
         int myAge = 17;
 
